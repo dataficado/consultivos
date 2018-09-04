b2ede014aaf5ba290c47914a573e8dc8d4b1c8e4
German Munoz <germanmunozbravo@gmail.com>
2018-09-04 15:41:58 -0500

    Incluye helpers.py para funciones y variables de uso comun. En proceso.

---
+++ consultivos/helpers.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+"""Modulo para variables y funciones de uso comun."""
+from pathlib import Path
+import logging
+
+
+def read_text(filepath):
+    """
+    Lee texto de archivo en filepath.
+
+    Parameters
+    ----------
+    filepath: str or Path
+
+    Returns
+    -------
+    str
+       Texto de archivo en filepath
+    """
+    try:
+        with open(filepath, encoding='utf-8') as f:
+                text = f.read()
+    except Exception as e:
+        logging.info(f'Error leyendo {filepath}: {e}')
+        text = ''
+
+    return text
