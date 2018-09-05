667c9401338f985153073f0c784447e0784fb56d
German Munoz <germanmunozbravo@gmail.com>
2018-09-05 17:34:00 -0500

    Funciones incluidas en helpers para ordenar iteración de filepaths.

--- consultivos/helpers.py
+++ consultivos/helpers.py
@@ -1,8 +1,41 @@
 # coding: utf-8
 """Modulo para variables y funciones de uso comun."""
+from pathlib import Path
 import logging
 
 import pandas as pd
+
+
+def ordered_filepaths(directory):
+    """
+    Parameters
+    ----------
+    directory: str or Path
+
+    Yields
+    ------
+    Path
+        Itera sobre cada documento en directory, devolviendo filepath del archivo.
+    """
+    cpath = Path(directory)
+    filepaths = sorted(cpath.glob('*.txt'))
+    for fpath in filepaths:
+        yield fpath
+
+
+def get_docnames(directory):
+    """
+    Parameters
+    ----------
+    directory: str or Path
+
+    Returns
+    -------
+    list of str
+        Itera sobre cada documento en directory, devolviendo nombre del archivo.
+    """
+
+    return [fpath.stem for fpath in ordered_filepaths(directory)]
 
 
 def read_text(filepath):
@@ -31,7 +64,7 @@ def read_text(filepath):
 def load_stopwords(filepath, sheet, col='word'):
     """
     Lee lista de palabras a usar como stopwords.
-    Palabras ubicadas en columna col de la hoja sheet.
+    Ubicadas en columna col de la hoja sheet del archivo Excel en filepath.
 
     Parameters
     ----------
